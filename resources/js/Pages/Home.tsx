c457add07358e13f5608cd8707bf537afa8b50fb
rakhahafizh <rakhah430@gmail.com>
2025-12-17 22:22:53 +0700

    Update Google Maps embed for Cikarang location
    - Replace placeholder map with actual Martabak Alim Cikarang location
    - Updated embed URL with correct coordinates

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -288,7 +288,7 @@ export default function Home() {
                   ></iframe>
                 ) : (
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0!2d107.1!3d-6.3!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTgnMDAuMCJTIDEwN8KwMDYnMDAuMCJF!5e0!3m2!1sen!2sid!4v1234567890"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7931.98649809519!2d107.1825953691671!3d-6.264617022326239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6984a2d0d4f555%3A0xddee7e1ad9bc6ab5!2sMARTABAK%20ALIM!5e0!3m2!1sen!2sid!4v1765984820396!5m2!1sen!2sid"
                     width="100%"
                     height="400"
                     style={{ border: 0, borderRadius: '12px' }}
