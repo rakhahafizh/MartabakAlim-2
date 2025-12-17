2da7b1db0af6c66a4a1394cd4700a1df3a621dab
rakhahafizh <rakhah430@gmail.com>
2025-12-18 01:04:10 +0700

    Fix return type for Inertia::location()
    - Added Illuminate\Http\Response import as IlluminateResponse
    - Changed return type from Response to IlluminateResponse
    - Fixes TypeError after login
    - Inertia::location() returns Illuminate response, not Inertia response

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as IlluminateResponse;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -23,7 +24,7 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): IlluminateResponse
     {
         $request->authenticate();
 
